2aa385c45050858f3fb13276014fd397544e152c
pwbear12300 <timewornbn@gmail.com>
2022-11-03 22:34:07 -0300

    feat: add home controller to the server

---
+++ image-capture/Server/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace image_capture.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+
+    }
+}
