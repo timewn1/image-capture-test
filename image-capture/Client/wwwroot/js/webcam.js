7a907899bd48b5aad719104716327fed386dd4ee
pwbear12300 <timewornbn@gmail.com>
2022-11-03 22:23:59 -0300

    feat: connect camera to the project

---
+++ image-capture/Client/wwwroot/js/webcam.js
@@ -0,0 +1,17 @@
+﻿function startVideo(src) {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
+            let video = document.getElementById(src);
+            if ("srcObject" in video) {
+                video.srcObject = stream;
+            } else {
+                video.src = window.URL.createObjectURL(stream);
+            }
+            video.onloadedmetadata = function (e) {
+                video.play();
+            };
+            video.style.webkitTransform = "scaleX(-1)";
+            video.style.transform = "scaleX(-1)";
+        });
+    }
+}
