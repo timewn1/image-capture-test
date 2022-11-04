a8731282825fe189e403ec74fe12fb8a165cf71c
pwbear12300 <timewornbn@gmail.com>
2022-11-03 23:51:55 -0300

    feat: add image save feature to the server

--- image-capture/Server/Controllers/HomeController.cs
+++ image-capture/Server/Controllers/HomeController.cs
@@ -1,27 +1,24 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using IronPdf;
+using System.IO;
+using System.Linq;
+using System.Reflection.Metadata;
 
-namespace image_capture.Server.Controllers
+namespace blazor_test_app.Server.Controllers
 {
+    public class ImageDataType
+    {
+        public string image { get; set; }
+    }
+
     [Route("api/[controller]")]
     [ApiController]
-    public class HomeController : ControllerBase
+    public class ImageController : Controller
     {
-        public class ImageDataType
+        [HttpPost("[action]")]
+        public void Send(ImageDataType json)
         {
-            public string image { get; set; }
-        }
 
-        [Route("api/[controller]")]
-        [ApiController]
-        public class ImageController : Controller
-        {
-            [HttpPost("[action]")]
-            public void Send(ImageDataType json)
-            {
-                
-            }
         }
-
     }
 }
